0c293a834f73c8e8dca01822662a9236b812c057
jannassl <jannassl@metropolia.fi>
2025-10-22 16:44:04 +0300

    Home sivun asettelua, ja dashboard komponenttien luonti

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,14 @@
-export const Dashboard = () => {
-    return (
-        <div>
-
-        </div>
-    )
+interface DashboardProps {
+  url?: string;
+  title?: string;
+  className?: string;
 }
+
+export const Dashboard = ({ url, title, className }: DashboardProps) => (
+  <iframe
+    src={url}
+    title={title || "Grafana Dashboard"}
+    className={className}
+    allowFullScreen
+  />
+);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,23 @@
+import { Dashboard } from "@/components/Dashboard";
+
 export const Home = () => {
-    return (
-        <div>
+  return (
+    <div className="min-h-screen w-auto flex flex-col justify-center items-center">
+      <div className="w-[80%] h-full m-0">
+        <div className="flex flex-col flex-1 gap-4 m-0">
+          <Dashboard
+            url="https://your-grafana/snapshot/..."
+            className=" w-full flex-1 rounded-2xl bg-Main-hit m-0 min-h-96"
+          />
+          <button className=" self-start m-0 p-1 flex-none h-12 rounded bg-blue-500 text-white">
+            Testi nappi
+          </button>
+          <Dashboard
+            url="https://your-grafana/snapshot/..."
+            className=" w-full flex-1 rounded-2xl bg-Main-piccolo m-0"
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
