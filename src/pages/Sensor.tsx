fd7c2f5176cd0742483ce7adad787c15e2a28f74
jannassl <jannassl@metropolia.fi>
2025-10-30 00:25:32 +0200

    Sensorikohtainen dashboard upotettu. Täytyy vielä muokata miten se renderöidään

--- src/pages/Sensor.tsx
+++ src/pages/Sensor.tsx
@@ -1,12 +1,19 @@
-//import {useParams} from "react-router-dom";
-
-export const SensorData = () => {
-    //const { sensorId } = useParams();
-
-    return (
-        <div>
-
-            <p>asdasdas</p>
-        </div>
-    )
+//import { useParams } from "react-router-dom";
+interface SensorDataProps {
+  dsb_link: String;
+  styles?: string;
 }
+
+export const SensorData = ({ styles }: { styles?: string }) => {
+  //const { sensorId } = useParams<{ sensorId: string }>();
+  const defaultClasses = "grow rounded-md shadow-light-shadow-sm h-full";
+  const dsb_link = `http://localhost:3000/dashboard/snapshot/v3UwaQsLVEH3dUdgQtnKhGJZZv9GjZVk`;
+
+  return (
+    <iframe
+      title="Dashboard"
+      src={dsb_link}
+      className={`${defaultClasses} ${styles}`}
+    ></iframe>
+  );
+};
