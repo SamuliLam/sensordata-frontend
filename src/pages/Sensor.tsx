8ff3e0b9c2845e3a5c5ba18234a0fdd103899bc3
jannassl <jannassl@metropolia.fi>
2025-10-30 09:33:36 +0200

    Muokattu snapshot käyttämään dashboardia sensor.tsx tiedostossa

--- src/pages/Sensor.tsx
+++ src/pages/Sensor.tsx
@@ -1,19 +1,17 @@
 //import { useParams } from "react-router-dom";
+import { Dashboard } from "@/components/Dashboard";
+
 interface SensorDataProps {
   dsb_link: String;
   styles?: string;
 }
 
 export const SensorData = ({ styles }: { styles?: string }) => {
-  //const { sensorId } = useParams<{ sensorId: string }>();
+  // Uncomment for dynamic sensorId in the future:
+  // const { sensorId } = useParams<{ sensorId: string }>();
+  // const dsb_link = `http://localhost:3000/dashboard/snapshot/${sensorId}`;
   const defaultClasses = "grow rounded-md shadow-light-shadow-sm h-full";
   const dsb_link = `http://localhost:3000/dashboard/snapshot/v3UwaQsLVEH3dUdgQtnKhGJZZv9GjZVk`;
 
-  return (
-    <iframe
-      title="Dashboard"
-      src={dsb_link}
-      className={`${defaultClasses} ${styles}`}
-    ></iframe>
-  );
+  return <Dashboard dsb_link={dsb_link} styles={"w-full h-full"} />;
 };
