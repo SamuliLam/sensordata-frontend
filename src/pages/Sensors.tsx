13c1df5308ba6257750ffa707e5deee16c6e6f8a
kevin <kevinvkalle@gmail.com>
2025-12-02 19:29:32 +0200

    Sensors dashboard showing temp and hum

--- src/pages/Sensors.tsx
+++ src/pages/Sensors.tsx
@@ -2,8 +2,10 @@ import {Dashboard} from "@/components/Dashboard.tsx";
 
 export const Sensors = () => {
     return (
-        <div className="dashboard-container flex flex-col gap-3 justify-center items-center grow h-full p-2">
-            <Dashboard styles={"w-full"} dsb_link={"http://localhost:3000/dashboard/snapshot/WnwuekcHO6jAIz85G2pFXp1ANhisryWc"}>
+        <div className="dashboard-container flex flex-col gap-3 justify-center items-center grow  w-full p-10">
+            <Dashboard
+                styles="w-full"
+                dsb_link={"http://localhost:3000/d/adxdfcs/humidity?orgId=1&from=2025-12-02T11:19:30.543Z&to=2025-12-02T17:19:30.543Z&timezone=browser&viewPanel=panel-1"}>
             </Dashboard>
         </div>
     )
